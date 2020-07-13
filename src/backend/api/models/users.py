f6e30ff8883f29210b876752aed843659bb9acc6
DIO0550 <diot12345@yahoo.co.jp>
2020-07-13 19:06:12 +0900

    少し調整。

--- src/backend/api/models/users.py
+++ src/backend/api/models/users.py
@@ -9,6 +9,8 @@ class User(db.Model):
 
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   name = db.Column(db.String(50), nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+  updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
 class UserSchema(ma.Schema):
   class Meta:
