1e04381f6f3cad6226f31df984adb649615fa072
DIO0550 <diot12345@yahoo.co.jp>
2020-08-30 20:31:15 +0900

    ビルド物追加

--- slack/build.sh
+++ slack/build.sh
@@ -1,1 +1,4 @@
 docker-compose run --rm frontend npm run build
+cp src/dist/build.js product/dist/build.js
+cp src/dist/build.js.LICENSE.txt product/dist/build.js.LICENSE.txt
+cp src/dist/index.html product/dist/index.html
